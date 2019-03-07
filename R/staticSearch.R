fd71e4d3b2efe897951d61b2e14201014f81015f
seokhoonj <seokhoonj@gmail.com>
2019-03-08 01:10:54 +0900

    modify staticSearch file

--- R/staticSearch.R
+++ R/staticSearch.R
@@ -4,21 +4,67 @@
 #' @param you can get korea economy data through the api
 #' @keywords ecos, staticSearch
 #' @examples
-#' df <- staticSearch(key = "sample", stat_code = "010Y002", cycle = "MM", start = "196001", end = "201812", item_code = "AAAA11")
+#' df <- staticSearch()
 #' head(df)
 #' @export
-staticSearch <- function(key, stat_code, count, cycle, start, end, item_code) {
-	url <- URLencode(paste0("http://ecos.bok.or.kr/api/StatisticSearch/", key, 
-							"/xml/kr/1/1000/", stat_code, "/", cycle, "/", start, "/", end, "/", item_code, "/?/?/"))
-	html <- getURLContent(url)
-	xml_all <- xmlParse(html)
-	xml_cnt <- xpathApply(xml_all, "//list_total_count")[[1]]
-	list_total_count <- as.integer(xmlToList(xml_cnt))
-	xml_row <- xpathApply(xml_all, "//row") 
-	df <- xmlToDataFrame(xml_row, stringsAsFactors = FALSE)
-	names(df) <- tolower(names(df))
-	df$time <- as.Date(paste0(as.character(df$time), "01"), format = "%Y%m%d")
-	df$data_value <- as.numeric(df$data_value)
-	attr(df, "list_total_count") <- list_total_count
+staticSearch <- function(key, format, lang, count, stat_code, cycle, start_date, end_date, item_code) {
+
+	if (missing(key))
+		key <- "LBVUDMTWICYRKCSJAYO6"
+
+	if (missing(format))
+		format <- "json"
+
+	if (missing(lang))
+		lang <- "kr"
+
+	if (missing(count))
+		count <- 1000
+
+	if (missing(stat_code))
+		stat_code <- "010Y002"
+	
+	if (missing(cycle))
+		cycle <- "MM"
+
+	if (missing(start_date))
+		start_date <- "196001"
+
+	if (missing(end_date))
+		end_date <- "201812"
+
+	if (missing(item_code))
+		item_code <- "AAAA11"
+
+	if (format == "json") {
+
+		url <- URLencode(sprintf("http://ecos.bok.or.kr/api/StatisticSearch/%s/%s/%s/1/%s/%s/%s/%s/%s/%s/?/?/", 
+								 key, format, lang, count, stat_code, cycle, start_date, end_date, item_code))
+		html <- getURLContent(url)
+		json_all <- fromJSON(html)
+		cnt  <- json_all$StatisticSearch[[1]]
+		df   <- json_all$StatisticSearch[[2]]
+		df[] <- lapply(df, trimws)
+		names(df) <- tolower(names(df))
+		df$time <- as.Date(paste0(df$time, "01"), format = "%Y%m%d")
+		df$data_value <- as.numeric(df$data_value)
+		attr(df, "list_total_count") <- cnt 
+
+	} else {
+
+		url <- URLencode(sprintf("http://ecos.bok.or.kr/api/StatisticSearch/%s/%s/%s/1/%s/%s/%s/%s/%s/%s/?/?/", 
+								 key, format, lang, count, stat_code, cycle, start_date, end_date, item_code))
+		html <- getURLContent(url)
+		xml_all <- xmlParse(html)
+		xml_cnt <- xpathApply(xml_all, "//list_total_count")[[1]]
+		cnt <- as.integer(xmlToList(xml_cnt))
+		xml_row <- xpathApply(xml_all, "//row") 
+		df <- xmlToDataFrame(xml_row, stringsAsFactors = FALSE)
+		names(df) <- tolower(names(df))
+		df$time <- as.Date(paste0(as.character(df$time), "01"), format = "%Y%m%d")
+		df$data_value <- as.numeric(df$data_value)
+		attr(df, "list_total_count") <- cnt 
+	}
+
 	return(df); gc()
 }
