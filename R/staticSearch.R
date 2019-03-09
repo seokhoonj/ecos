a07ff67a13963a00453d431d5d8a5d9fbadde65d
seokhoonj <seokhoonj@gmail.com>
2019-03-09 17:11:03 +0900

    add format exception

--- R/staticSearch.R
+++ R/staticSearch.R
@@ -47,15 +47,15 @@ staticSearch <- function(api_key, format, lang, count, stat_code, cycle, start_d
 								 api_key, format, lang, count, stat_code, cycle, start_date, end_date, item_code))
 		html <- getURLContent(url)
 		json_all <- fromJSON(html)
-		cnt  <- json_all$StatisticSearch[[1]]
-		df   <- json_all$StatisticSearch[[2]]
+		cnt  <- json_all$StatisticSearch$list_total_count
+		df   <- json_all$StatisticSearch$row
 		df[] <- lapply(df, trimws)
 		names(df) <- tolower(names(df))
 		df$time <- as.Date(paste0(df$time, "01"), format = "%Y%m%d")
 		df$data_value <- as.numeric(df$data_value)
 		attr(df, "list_total_count") <- cnt 
 
-	} else {
+	} else if {
 
 		url <- URLencode(sprintf("http://ecos.bok.or.kr/api/StatisticSearch/%s/%s/%s/1/%s/%s/%s/%s/%s/%s/?/?/", 
 								 api_key, format, lang, count, stat_code, cycle, start_date, end_date, item_code))
@@ -69,6 +69,10 @@ staticSearch <- function(api_key, format, lang, count, stat_code, cycle, start_d
 		df$time <- as.Date(paste0(as.character(df$time), "01"), format = "%Y%m%d")
 		df$data_value <- as.numeric(df$data_value)
 		attr(df, "list_total_count") <- cnt 
+	} else {
+
+		stop("not supported data format.")
+
 	}
 
 	return(df); gc()
